e581146c43b6f22c27238f98a9caf681575c64fa
Paweł Dąbrowski <pawel.dabrowski@ironin.pl>
2024-01-17 13:57:52 +0100

    Test cases for metadata

--- spec/metadata_spec.rb
+++ spec/metadata_spec.rb
@@ -4,121 +4,164 @@ RSpec.describe 'Check system metadata for subscriptions and customers' do
   it 'creates metafield, adds metadata, and lists subscriptions with metadata' do
     client = build_client
 
-    # result = client.custom_fields.create_metafields(
-    #   AdvancedBilling::ResourceType::SUBSCRIPTIONS,
-    #   body: AdvancedBilling::CreateMetafieldsRequest.new(
-    #     AdvancedBilling::CreateMetafield.new(
-    #       "Dropdown field #{SecureRandom.hex(4)}",
-    #       AdvancedBilling::MetafieldScope.new(
-    #         nil,
-    #         nil,
-    #         nil,
-    #         nil,
-    #         AdvancedBilling::IncludeOption::INCLUDE,
-    #         AdvancedBilling::IncludeOption::INCLUDE
-    #       ),
-    #       AdvancedBilling::MetafieldInput::DROPDOWN,
-    #       [
-    #         'option 1',
-    #         'option 2'
-    #       ]
-    #     )
-    #   )
-    # )
+    dropdown_name = "Dropdown field #{SecureRandom.hex(4)}"
+    text_field_name = "Text field #{SecureRandom.hex(4)}"
 
-    # text_field_name = "Text field #{SecureRandom.hex(4)}"
+    result = client.custom_fields.create_metafields(
+      AdvancedBilling::ResourceType::SUBSCRIPTIONS,
+      body: AdvancedBilling::CreateMetafieldsRequest.new(
+        [
+          AdvancedBilling::CreateMetafield.new(
+            dropdown_name,
+            AdvancedBilling::MetafieldScope.new(
+              nil,
+              nil,
+              nil,
+              nil,
+              AdvancedBilling::IncludeOption::INCLUDE,
+              AdvancedBilling::IncludeOption::INCLUDE
+            ),
+            AdvancedBilling::MetafieldInput::DROPDOWN,
+            [
+              'option 1',
+              'option 2'
+            ]
+          ),
+          AdvancedBilling::CreateMetafield.new(
+            text_field_name
+          )
+        ]
+      )
+    )
 
-    # result = client.custom_fields.create_metafields(
-    #   AdvancedBilling::ResourceType::SUBSCRIPTIONS,
-    #   body: AdvancedBilling::CreateMetafieldsRequest.new(
-    #     AdvancedBilling::CreateMetafield.new(
-    #       text_field_name
-    #     )
-    #   )
-    # )
+    expect(result.size).to eq(2)
 
-    # text_field = result.first
+    select_field = result.find { |res| res.input_type == 'dropdown' }
 
-    # expect(text_field.input_type).to eq('text')
-    # expect(text_field.enum).to eq(nil)
+    expect(select_field.enum).to eq(['option 1', 'option 2'])
+    expect(select_field.scope.public_edit).to eq('1')
+    expect(select_field.scope.public_show).to eq('1')
+    expect(select_field.scope.hosted).to eq([])
+    expect(select_field.scope.invoices).to eq('0')
+    expect(select_field.scope.portal).to eq('0')
+    expect(select_field.scope.statements).to eq('0')
+    expect(select_field.data_count).to eq(0)
+    expect(select_field.name).to eq(dropdown_name)
 
-    # result = client.custom_fields.create_metafields(
-    #   AdvancedBilling::ResourceType::SUBSCRIPTIONS,
-    #   body: [
-    #     AdvancedBilling::CreateMetafieldsRequest.new(
-    #       AdvancedBilling::CreateMetafield.new(
-    #         "Dropdown field #{SecureRandom.hex(4)}",
-    #         AdvancedBilling::MetafieldScope.new(
-    #           nil,
-    #           nil,
-    #           nil,
-    #           nil,
-    #           AdvancedBilling::IncludeOption::INCLUDE,
-    #           AdvancedBilling::IncludeOption::INCLUDE
-    #         ),
-    #         AdvancedBilling::MetafieldInput::DROPDOWN,
-    #         [
-    #           'option 1',
-    #           'option 2'
-    #         ]
-    #       )
-    #     ),
-    #     AdvancedBilling::CreateMetafieldsRequest.new(
-    #       AdvancedBilling::CreateMetafield.new(
-    #         "Text field #{SecureRandom.hex(4)}"
-    #       )
-    #     )
-    #   ]
-    # )
+    text_field = result.find { |res| res.input_type == 'text' }
 
-    # result = client.custom_fields.create_metafields(
-    #   AdvancedBilling::ResourceType::CUSTOMERS,
-    #   body: AdvancedBilling::CreateMetafieldsRequest.new(
-    #     AdvancedBilling::CreateMetafield.new(
-    #       "Radio field #{SecureRandom.hex(4)}",
-    #       AdvancedBilling::MetafieldScope.new(
-    #         AdvancedBilling::IncludeOption::INCLUDE,
-    #         AdvancedBilling::IncludeOption::INCLUDE,
-    #         AdvancedBilling::IncludeOption::INCLUDE,
-    #       ),
-    #       AdvancedBilling::MetafieldInput::RADIO,
-    #       [
-    #         'option 1',
-    #         'option 2'
-    #       ]
-    #     )
-    #   )
-    # )
+    expect(text_field.enum).to eq(nil)
+    expect(text_field.scope.public_edit).to eq('0')
+    expect(text_field.scope.public_show).to eq('0')
+    expect(text_field.scope.hosted).to eq([])
+    expect(text_field.scope.invoices).to eq('0')
+    expect(text_field.scope.portal).to eq('0')
+    expect(text_field.scope.statements).to eq('0')
+    expect(text_field.data_count).to eq(0)
+    expect(text_field.name).to eq(text_field_name)
 
-    # result = client.custom_fields.list_metadata(
-    #   'resource_type' => AdvancedBilling::ResourceType::SUBSCRIPTIONS,
-    #   'resource_id' => '322944'
-    # )
-    
-    # result = client.custom_fields.list_metafields(
-    #   'resource_type' => AdvancedBilling::ResourceType::SUBSCRIPTIONS,
-    # )
+    radio_name = "Radio field #{SecureRandom.hex(4)}"
 
-    # result = client.custom_fields.create_metadata(
-    #   AdvancedBilling::ResourceType::SUBSCRIPTIONS,
-    #   '322944', # resource_id, subscription
-    #   body: AdvancedBilling::CreateMetadataRequest.new(
-    #     [
-    #       AdvancedBilling::CreateMetadata.new(
-    #         'Text field a2303bed',
-    #         'test1'
-    #       )
-    #     ]
-    #   )
-    # )
+    result = client.custom_fields.create_metafields(
+      AdvancedBilling::ResourceType::CUSTOMERS,
+      body: AdvancedBilling::CreateMetafieldsRequest.new(
+        AdvancedBilling::CreateMetafield.new(
+          radio_name,
+          AdvancedBilling::MetafieldScope.new(
+            AdvancedBilling::IncludeOption::INCLUDE,
+            AdvancedBilling::IncludeOption::INCLUDE,
+            nil,
+            AdvancedBilling::IncludeOption::INCLUDE,
+          ),
+          AdvancedBilling::MetafieldInput::RADIO,
+          [
+            'option 1',
+            'option 2'
+          ]
+        )
+      )
+    )
 
-    # result = client.subscriptions.list_subscriptions(
-    #   'metadata' => {
-    #     'Text field a2303bed' => 'test1'
-    #   }
-    # )
+    expect(result.size).to eq(1)
 
-    # puts result.size
+    radio_field = result.first
+
+    expect(radio_field.enum).to eq(['option 1', 'option 2'])
+    expect(radio_field.scope.public_edit).to eq('0')
+    expect(radio_field.scope.public_show).to eq('0')
+    expect(radio_field.scope.hosted).to eq([])
+    expect(radio_field.scope.invoices).to eq('1')
+    expect(radio_field.scope.portal).to eq('1')
+    expect(radio_field.scope.statements).to eq('0')
+    expect(radio_field.scope.csv).to eq('1')
+    expect(radio_field.data_count).to eq(0)
+    expect(radio_field.name).to eq(radio_name)
+
+    product_family = client.product_families.read_product_family('handle:developer-experience-billing-plans').product_family
+    customer = Factories::Customer.create(client)
+    product = Factories::Product.create(client, product_family: product_family)
+    subscription = Factories::Subscription.create(client, customer: customer, product: product)
+
+    results = client.custom_fields.create_metadata(
+      AdvancedBilling::ResourceType::SUBSCRIPTIONS,
+      subscription.id,
+      body: AdvancedBilling::CreateMetadataRequest.new(
+        [
+          AdvancedBilling::CreateMetadata.new(
+            dropdown_name,
+            'option 1'
+          ),
+          AdvancedBilling::CreateMetadata.new(
+            text_field_name,
+            'something'
+          )
+        ]
+      )
+    )
+
+    expect(results.size).to eq(2)
+
+    select_metafield = results.find { |res| res.metafield_id == select_field.id }
+
+    expect(select_metafield.value).to eq('option 1')
+    expect(select_metafield.resource_id).to eq(subscription.id)
+    expect(select_metafield.name).to eq(dropdown_name)
+
+    text_metafield = results.find { |res| res.metafield_id == text_field.id }
+
+    expect(text_metafield.value).to eq('something')
+    expect(text_metafield.resource_id).to eq(subscription.id)
+    expect(text_metafield.name).to eq(text_field_name)
+
+    results = client.subscriptions.list_subscriptions(
+      'metadata' => {
+        dropdown_name => 'option 1'
+      }
+    )
+
+    expect(results.size).to eq(1)
+    expect(results.first.subscription.id).to eq(subscription.id)
+
+    results = client.custom_fields.create_metadata(
+      AdvancedBilling::ResourceType::CUSTOMERS,
+      customer.id,
+      body: AdvancedBilling::CreateMetadataRequest.new(
+        [
+          AdvancedBilling::CreateMetadata.new(
+            radio_name,
+            'option 2'
+          )
+        ]
+      )
+    )
+
+    expect(results.size).to eq(1)
+
+    radio_metafield = results.first
+
+    expect(radio_metafield.value).to eq('option 2')
+    expect(radio_metafield.resource_id).to eq(customer.id)
+    expect(radio_metafield.name).to eq(radio_name)
   end
 end
 
