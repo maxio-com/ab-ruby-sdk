2bead36ffb54ee1403a550cd14a9f7eb24482c53
Paweł Dąbrowski <pawel.dabrowski@ironin.pl>
2024-01-17 11:30:32 +0100

    Update test cases after SDK update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,10 +17,8 @@ def build_client(api_key: nil, subdomain: nil, domain: nil)
   local_conf = Dotenv.parse('.env.test')
 
   AdvancedBilling::Client.new(
-    basic_auth_credentials: AdvancedBilling::BasicAuthCredentials.new(
-      username: api_key || ENV.fetch('API_KEY', local_conf['API_KEY']),
-      password: 'x'
-    ),
+    basic_auth_user_name: api_key || ENV.fetch('API_KEY', local_conf['API_KEY']),
+    basic_auth_password: 'x',
     environment: AdvancedBilling::Environment::PRODUCTION,
     subdomain: subdomain || ENV.fetch('SUBDOMAIN', local_conf['SUBDOMAIN']),
     domain: domain || ENV.fetch('DOMAIN', local_conf['DOMAIN'])
