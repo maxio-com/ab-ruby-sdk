aaf0538f5e10a82c768404d7176dbe5a36522543
Paweł Dąbrowski <pawel.dabrowski@ironin.pl>
2024-01-19 08:21:45 +0100

    Ensure that metered component name is always unique

--- spec/factories/metered_component.rb
+++ spec/factories/metered_component.rb
@@ -1,12 +1,14 @@
 module Factories
   class MeteredComponent
     def self.create(client, product_family:)
+      token = SecureRandom.hex(6)
+
       client.components.create_component(
         product_family.id,
         AdvancedBilling::ComponentKindPath::METERED_COMPONENTS,
         body: AdvancedBilling::CreateMeteredComponent.new(
           AdvancedBilling::MeteredComponent.new(
-            FFaker::Product.product_name,
+            "#{FFaker::Product.product_name}#{token}",
             FFaker::Lorem.word,
             AdvancedBilling::PricingScheme::STAIRSTEP,
             nil,
