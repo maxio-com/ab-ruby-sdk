00b980d6c153216a0d9dcd5b28b67e6359078a76
Maciej Nedza <maciej.nedza@keen.io>
2024-02-19 10:47:21 +0100

    [DE-728] Update client authentication in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,8 +23,10 @@ def build_client(api_key: nil, subdomain: nil, domain: nil)
   local_conf = Dotenv.parse('.env.test')
 
   AdvancedBilling::Client.new(
-    basic_auth_user_name: api_key || ENV.fetch('API_KEY', local_conf['API_KEY']),
-    basic_auth_password: 'x',
+    basic_auth_credentials: AdvancedBilling::BasicAuthCredentials.new(
+      username: api_key || ENV.fetch('API_KEY', local_conf['API_KEY']),
+      password: 'x'
+    ),
     environment: AdvancedBilling::Environment::PRODUCTION,
     subdomain: subdomain || ENV.fetch('SUBDOMAIN', local_conf['SUBDOMAIN']),
     domain: domain || ENV.fetch('DOMAIN', local_conf['DOMAIN'])
